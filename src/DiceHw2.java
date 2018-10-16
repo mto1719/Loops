d0670f090dd0ff6b5433b55fea9aad214be20fbe
mto1719 <42977020+mto1719@users.noreply.github.com>
2018-10-16 12:35:55 -0400

    Update DiceHw2.java

--- src/DiceHw2.java
+++ src/DiceHw2.java
@@ -1,9 +1,24 @@
 
 public class DiceHw2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {	
+             
+		numRolls dice1 = new numRolls()
+		numRolls dice2 = new numRolls();
+				int ROLL1 = dice1.roll();
+				int ROLL2 = dice2.roll();
+				System.out.println(ROLL1);
+				System.out.println(ROLL2);
+				int count = 1;
+				while (ROLL1 != ROLL2)
+					{
+					ROLL1 = dice1.roll();
+					ROLL2 = dice2.roll();
+					System.out.println(ROLL1);
+					System.out.println(ROLL2);
+					count++;
+					}
+				System.out.print(count);
 	}
 
 }
